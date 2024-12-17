49f3b27488370615801e4acbdcc354766e917300
Dnyaneshwari Shinde <55287328+Dnyaneshwari-shinde@users.noreply.github.com>
2024-12-17 22:35:51 +0530

    update the index.js file for import statement

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,23 @@
-const express = require("express");
+import express from "express";
 const app = express();
+
+import bodyParser from "body-parser";
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+import cors from "cors";
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT");
+  next();
+});
+
 
 app.get("/", (req, res) => {
     res.send("Backend is running!");
